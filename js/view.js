e7258933103e98e48830e4e9403b102ac6c89544
tmaeda1981jp <tmaeda1981jp@gmail.com>
2013-11-30 17:23:14 +0900

    結果画面にchartを表示するようにした．

--- js/view.js
+++ js/view.js
@@ -1,16 +1,37 @@
 Questionnaire.ResultView = Ember.View.extend({
   didInsertElement: function() {
-    console.log("hoge");
+    var content = this.get("controller.content");
+    this.showQ1Result(content);
+    this.showQ2Result(content);
+    this.showQ3Result(content);
+  },
+
+  showQ1Result: function(content) {
     var ctx = this.$('#result_q1')[0].getContext('2d');
     new Chart(ctx).Bar({
       labels: ["楽しかった", "楽しくなかった"],
-      datasets: [
-        {
-          fillColor: "rgba(220,220,220,0.5)",
-          strokeColor : "rgba(220,220,220,1)",
-			    data : [65, 22]
-        }
-      ]
+      datasets: [{
+        fillColor: "rgba(220,220,220,0.5)",
+        strokeColor : "rgba(220,220,220,1)",
+        data: [
+          content.filterBy("isEnjoy", true).length,
+          content.filterBy("isEnjoy", false).length
+        ]
+      }]
     });
+  },
+
+  showQ2Result: function(content) {
+    var ctx = this.$('#result_q2')[0].getContext('2d');
+    new Chart(ctx).Pie([
+        {value: content.filterBy("selectedSatisfyPoint", 1).length, color:"#f00"},
+        {value: content.filterBy("selectedSatisfyPoint", 2).length, color:"#0f0"},
+        {value: content.filterBy("selectedSatisfyPoint", 3).length, color:"#00f"},
+        {value: content.filterBy("selectedSatisfyPoint", 4).length, color:"#ff0"},
+        {value: content.filterBy("selectedSatisfyPoint", 5).length, color:"#0ff"}
+    ]);
+  },
+
+  showQ3Result: function(content) {
   }
 });
